d0697ba15cb73f38470f1c7ddb4f74781875e0a6
jeon-seongjae <tjdwoek62@google.com>
2021-08-12 23:56:38 +0900

    postive i can do

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
   @Post()
-  postUsers() {}
+  postUsers(@Body() data: JoinRequestDto) {
+    this.usersService.postUsers(data.email, data.nickname, data.password);
+    return;
+  }
 
   @Post('login')
-  login() {
+  login(@Req() req) {
     return req.user;
   }
 
