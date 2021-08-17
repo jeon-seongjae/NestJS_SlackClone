a2224525f680cb740828c959a00a1f368f2acbc0
jeon-seongjae <tjdwoek62@google.com>
2021-08-17 23:31:19 +0900

    i can do it jeongit status

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -1,7 +1,30 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { urlencoded } from 'express';
 
+@ApiTags('DM')
 @Controller('api/workspace/:url/dms')
 export class DmsController {
+  @ApiParam({
+    name: 'url',
+    required: true,
+    description: '워크스페이스 url',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: '사용자 아이디',
+  })
+  @ApiQuery({
+    name: 'perPage',
+    required: true,
+    description: '한 번에 가져오는 개수',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: true,
+    description: '불러올 페이지',
+  })
   @Get(':id/chats')
   getChat(@Query() query, @Param() param) {
     // @Query('perPage') perPage, @Query('page') page // 위에 쿼리를 전체 다받는 걸로 써도 되고 이렇게 따로 받게 할 수도 있다.
